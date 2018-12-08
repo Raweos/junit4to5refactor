b1cbb06b1c442f6b3da8a48793d69a1e49f91c38
Raweos <kosa.095@gmail.com>
2018-12-08 14:23:46 +0100

    update

--- src/test/java/com/example/extensions/TemporaryDirectoryExtension.java
+++ src/test/java/com/example/extensions/TemporaryDirectoryExtension.java
@@ -1,17 +1,39 @@
 package com.example.extensions;
 
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 
 public class TemporaryDirectoryExtension {
 
-
     private void deleteRecursively(Path root) throws IOException {
-        throw new UnsupportedOperationException();
+        Files.walkFileTree(root, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                System.out.println("Deleting file " + file);
+                Files.delete(file);
+                return super.visitFile(file, attrs);
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                System.out.println("Deleting file " + dir);
+                Files.delete(dir);
+                return super.postVisitDirectory(dir, exc);
+            }
+        });
     }
 
     private Path createTempDir() {
-        throw new UnsupportedOperationException();
+        try {
+            return Files.createTempDirectory("junit");
+        } catch (IOException ex) {
+            throw new UnsupportedOperationException();
+            //throw new ParameterResolutionException("Could not create temp dir", ex);
+        }
     }
 }
